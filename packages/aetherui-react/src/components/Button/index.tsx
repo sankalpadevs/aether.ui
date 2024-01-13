adb69618fcb3e1332b4a058eb70640d1672ba8f6
chitwan001 <chitwan001@gmail.com>
2024-01-13 20:04:36 +0530

    fix: linting fixes

--- packages/aetherui-react/src/components/Button/index.tsx
+++ packages/aetherui-react/src/components/Button/index.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 
 export const Button = () => {
-    return(
-        <button onClick={() => {
-            console.log('clicked!')
-        }}>Click me!</button>
-    )
+  return (
+    <button
+      onClick={() => {
+        console.log("clicked!");
+      }}
+    >
+      Click me!
+    </button>
+  );
 };
